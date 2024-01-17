ef783926af156021138369cba84aa9d0b6e90a8c
katriellucas <katriel.developer@gmail.com>
2024-01-17 16:40:53 -0300

    remove types

--- src/types/Backdrop.d.ts
+++
@@ -1,5 +0,0 @@
-export declare global {
-	type Backdrop = {
-		key: string;
-	};
-}
--- src/types/Buttons.d.ts
+++
@@ -1,9 +0,0 @@
-export declare global {
-	type CommonButton = {
-		href?: string;
-		target?: '_blank' | '_self' | '_parent' | '_top';
-		onclick?: Function;
-		label: string;
-		type?: 'button' | 'submit' | 'reset';
-	};
-}
